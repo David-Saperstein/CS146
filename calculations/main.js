344a0e43fa3451645ea8b576fb3dea78b3cc2034
David-Saperstein <54864170+David-Saperstein@users.noreply.github.com>
2023-12-07 20:10:09 -0500

    added tax calc

--- calculations/main.js
+++ calculations/main.js
@@ -1,5 +1,4 @@
 const titles = document.getElementsByTagName("h1");
-console.log(titles);
 for(let i = 0; i < titles.length; i++){
     titles[i].style.transform = `rotate(${i % 2 == 0 ? '' : '-'}5deg)`;
 }
--- calculations/tax.js
+++ calculations/tax.js
@@ -1,32 +1,37 @@
-var annualIncome;
-//Single or Married(filing jointly) dropdown
-var filingStatus;
-
-//to calculate have function that takes income and status
-// set taxAmount = 0.0
-//First use the single status numbers
-//use if statements to go through each level
-/*
-if income > $243,725
-    taxAmount = taxAmount + (income-243725)*.35
-    income = 243725
-if income > 191,950
-    tax = tax + (income - 191,950)*.24
-...(continue using numbers at bottom of pages for single)
-(if the amount of income is < 11,600, taxAmount should return 0 )
-*/
-//make an if statement for if single and if married
-//copy and paste single to married and just change the money numbers
-//return taxAmount
-//output taxAmount next to calculations on tax.html
-
-taxCalc(income,stats)
-{
-    taxAmount = 0
-    return taxAmount;
+function taxCalc(income, mult){
+    let rate;
+    switch(true){
+        case (income > 243725 * mult):
+            rate = 35;
+            break;
+        case (income > 191950 * mult):
+            rate = 32;
+            break;
+        case (income > 100525 * mult):
+            rate = 24;
+            break;
+        case (income > 47150 * mult):
+            rate = 22;
+            break;
+        case (income > 11600 * mult):
+            rate = 12;
+            break;
+        default:
+            rate = 0;
+            break;
+    }
+    let result = income * rate / 100;
+    return result ? result.toFixed(2) : "???";
 }
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const income = urlParams.get("income");
+const mult = urlParams.get("filing-status");
 
+document.getElementById("status").value = mult;
+document.getElementById("income").value = income;
+document.getElementById("result").innerHTML = taxCalc(income, mult);
 
 //Tax percentages
 /*35% for incomes over $243,725 ($487,450 for married couples filing jointly)
