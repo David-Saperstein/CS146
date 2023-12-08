fc6d54cc4145136b7b20a0c28f8507c41572e1c2
David-Saperstein <54864170+David-Saperstein@users.noreply.github.com>
2023-12-07 19:00:33 -0500

    added mortgage calculation functionality

--- calculations/mortgage.js
+++ calculations/mortgage.js
@@ -1,12 +1,14 @@
-var mortgageAmount;
-//15, 20, or 30
-var loanTerm;
-var interestRate;
-
-//hopefully this basic code makes sense, its just a simple calculation
-//output the function output next to calculations in mortgage
-
-mortgageCalc(amount,term,rate)
-{
-return ((amount*(rate/100))/term)+(amount/term);
+function mortgageCalc(amount, term, rate){
+    let result = ((((amount * (rate / 100)) / term) + (amount / term)) * 100);
+    return result ? result.toFixed(2) : "???";
 }
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const amount = urlParams.get("mortgageAmount");
+const term = urlParams.get("lo");
+const rate = urlParams.get("ir");
+document.getElementById("amount").value = amount;
+document.getElementById("lo").value = term;
+document.getElementById("ir").value = rate;
+document.getElementById("result").innerHTML = mortgageCalc(amount, term, rate) + "/month";
