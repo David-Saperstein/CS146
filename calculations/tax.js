91c02a197044e1c52b98767e05193575ae0c3e14
Jae <84739325+jaejang02@users.noreply.github.com>
2023-12-10 14:14:44 -0500

    Putting state input in tax section

--- calculations/tax.js
+++ calculations/tax.js
@@ -1,5 +1,59 @@
-function taxCalc(income, mult){
+function taxCalc(income, mult, state){
     let rate;
+    let state_tax = {
+        'AK': 0.00,
+        'FL' : 0.00,
+        'NV' : 0.00,
+        'NH': 0.00,
+        'SD' : 0.00,
+        'TN' : 0.00,
+        'TX' : 0.00,
+        'WA' : 0.00,
+        'WY': 0.00,
+        'ND' : 0.0112,
+        'OH' : 0.0184,
+        'AZ' : 0.0244,
+        'RI' : 0.0302,
+        'PA' : 0.0307,
+        'LA' : 0.0315,
+        'IN' : 0.0318,
+        'NJ' : 0.0322,
+        'NM' : 0.0359,
+        'CT' : 0.0368,
+        'VT' : 0.0369,
+        'CO' : 0.0371,
+        'WI' : 0.0392,
+        'MI' : 0.0395,
+        'MS' : 0.0398,
+        'OK' : 0.0398,
+        'NC' : 0.0408,
+        'MO': 0.0413,
+        'MD': 0.0430,
+        'CA' : 0.0436,
+        'ID' : 0.0457,
+        'KS': 0.0458,
+        'AL' : 0.0466,
+        'MA' : 0.0469,
+        'WV' : 0.0471,
+        'WA' : 0.0473,
+        'KY' : 0.0475,
+        'ME' : 0.0476,
+        'NE' : 0.0476,
+        'IL' : 0.0478,
+        'DE' : 0.0483,
+        'UT' : 0.0490,
+        'GA' : 0.0490,
+        'SC' : 0.0493,
+        'VA' : 0.0494,
+        'AR' : 0.0502,
+        'MT' : 0.0522,
+        'IA' : 0.0543,
+        'HI' : 0.0679,
+        'OR' : 0.0805
+    }
+
+    let state_cut = income * state_tax[state];
+    console.log(state_cut);
     switch(true){
         case (income > 243725 * mult):
             rate = 35;
@@ -20,7 +74,8 @@ function taxCalc(income, mult){
             rate = 0;
             break;
     }
-    let result = income * rate / 100;
+    let base = income * rate / 100;
+    result = base + state_cut;
     return result ? result.toFixed(2) : "???";
 }
 
@@ -28,10 +83,11 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const income = urlParams.get("income");
 const mult = urlParams.get("filing-status");
-
+const state_rate = urlParams.get("states");
 document.getElementById("status").value = mult;
 document.getElementById("income").value = income;
-document.getElementById("result").innerHTML = taxCalc(income, mult);
+document.getElementById("states").value = state_rate;
+document.getElementById("result").innerHTML = taxCalc(income, mult, state_rate);
 
 //Tax percentages
 /*35% for incomes over $243,725 ($487,450 for married couples filing jointly)
